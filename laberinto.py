9f1851a0f34a16caf0b210fe2730cfd8894d90cc
luciacantos <luciacantos22@gmail.com>
2022-12-02 23:56:06 +0100

    bucle columna

--- laberinto.py
+++ laberinto.py
@@ -4,10 +4,6 @@ def laberinto(dimension, muros):
 
     for i in range(dimension):
         fila = []
-
-
-
-
-muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
-x = laberinto(5, muro)
-print(x)
+        for j in range(dimension):
+            fila.append(" ")
+            laberinto.append(fila)
