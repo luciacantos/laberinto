3fb77859e5890e1a49abba90256cfe3bf235ce48
luciacantos <luciacantos22@gmail.com>
2022-12-02 23:51:22 +0100

    bucle filas

--- laberinto.py
+++ laberinto.py
@@ -1,4 +1,13 @@
 def laberinto(dimension, muros):
     #creamos lista
     laberinto = []
-    
+
+    for i in range(dimension):
+        fila = []
+
+
+
+
+muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
+x = laberinto(5, muro)
+print(x)
