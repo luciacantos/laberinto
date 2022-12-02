2444f0bb79b8bf5dbc26afa6f640c9280c1df7cd
luciacantos <luciacantos22@gmail.com>
2022-12-02 23:59:38 +0100

    asignación de símbolos para las casillas

--- laberinto.py
+++ laberinto.py
@@ -7,3 +7,10 @@ def laberinto(dimension, muros):
         for j in range(dimension):
             fila.append(" ")
             laberinto.append(fila)
+
+    for muro in muros:
+        laberinto[muro[0]][muro[1]] = "X"
+
+        laberinto[dimension-1][dimension-1] = "S"
+
+        return laberinto
