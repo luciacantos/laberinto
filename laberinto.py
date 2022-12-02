626208d704b5813809083c7bb9eed1390e63179f
luciacantos <luciacantos22@gmail.com>
2022-12-03 00:03:46 +0100

    modificación

--- laberinto.py
+++ laberinto.py
@@ -9,11 +9,11 @@ def laberinto(dimension, muros):
         laberinto.append(fila)
 
     for muro in muros:
-        laberinto[muro[0]][muro[1]] = "X"
+        laberinto[muro[0]][muro[1]] = 'X'
 
-        laberinto[dimension-1][dimension-1] = "S"
+    laberinto[dimension-1][dimension-1] = 'S'
 
-        return laberinto
+    return laberinto
 
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
 x = laberinto(5,muro)
